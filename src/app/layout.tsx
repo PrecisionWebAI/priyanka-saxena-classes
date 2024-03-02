ca892f9fb7f8044f1d6fd9ab1fd94fecf2e210ae
Aditya Jain <ajain.adityajain@gmail.com>
2024-03-02 12:02:37 +0530

    added why pritanka classes section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="bg-white flex flex-col overflow-auto">
+        <div className="bg-white flex flex-col">
           <StaticNavbar />
           <StickyNavbar />
         </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 import Banner from "./Banner";
+import Image from "next/image";
 
 type Props = {};
+
+const whyChoosePCData = [
+  {
+    text: "Studio Recorded Classes",
+    image: "/images/why-pc/studio.webp",
+  },
+  {
+    text: "Supportive Team",
+    image: "/images/why-pc/supportive-team.webp",
+  },
+  {
+    text: "Academic Instructure Team",
+    image: "/images/why-pc/academic-instructor.webp",
+  },
+  {
+    text: "Study Material Covering all MTP/RTP",
+    image: "/images/why-pc/study-material.webp",
+  },
+  {
+    text: "Daily Doubt Session",
+    image: "/images/why-pc/daily-doubt.webp",
+  },
+  {
+    text: "Unlimited Views",
+    image: "/images/why-pc/unlimited-views.webp",
+  },
+];
 
 const Home = (props: Props) => {
   return (
@@ -9,6 +37,23 @@ const Home = (props: Props) => {
       <Banner />
       <div className="bg-accent text-white text-4xl font-bold flex w-full justify-center py-5">
         Face to Face New Batch Started at Laxmi Nagar
+      </div>
+      <div className="flex flex-col gap-3 justify-center items-center">
+        <span className="text-black text-2xl pb-5 pt-6 font-semibold">
+          WHY CHOOSE CLASSES FROM PRIYANKA CLASSES
+        </span>
+        <div className="flex gap-4">
+          {whyChoosePCData.map((pcd) => (
+            <Image
+              src={pcd.image}
+              alt={pcd.text}
+              key={pcd.text}
+              width={165}
+              height={95}
+              className="bg-black rounded-lg"
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
